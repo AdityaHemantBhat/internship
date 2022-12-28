cd8406cceb984185956022ca47d89a49936008d6
Aditya Bhat <120257459+AdityaHemantBhat@users.noreply.github.com>
2022-12-28 21:27:30 +0530

    Delete port directory

--- port/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-//php connection to database
-session_start();
-$servername ='localhost';
-$username ='root';
-$pass ='';
-$database ='port';
-$conn = mysqli_connect($servername,$username, $pass, $database) or die ("Error". mysqli_connect_error());
-?>
--- port/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-//log out from the website but the data remains as it is!
-session_start();
-unset($_SESSION['email']);
-unset($_SESSION['password']);
-header('location:login.php');
-die();
-?>
